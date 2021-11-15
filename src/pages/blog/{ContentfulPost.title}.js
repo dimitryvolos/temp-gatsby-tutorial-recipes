449bde669bec450dc3e3e0ad0ba6abf72fa3bfd6
dimitryvolos <59628805+dimitryvolos@users.noreply.github.com>
2021-11-15 20:53:16 +0000

    first commit

---
+++ src/pages/blog/{ContentfulPost.title}.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const PostTemplate = props => {
+  console.log(props)
+  return (
+    <div>
+      <h4>Post Template</h4>
+    </div>
+  )
+}
+
+export default PostTemplate
